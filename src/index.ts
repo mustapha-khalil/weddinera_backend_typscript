8d80cc56cb0bf9213f2447fa4c4ee39bb374cc92
Mustafa <mustafakh1997@gmail.com>
2023-03-12 22:12:02 +0200

    added imports for models to include their schemas in the database
    did that to fix schema not initialized error upon populate on a different model

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,30 @@ import cors from "cors";
 import http from "http";
 import notFound from "./middlewares/not-found";
 import errorMiddleware from "./middlewares/error";
+import reservationRoutes from "./routes/reservation.route";
 import userRoutes from "./routes/user.route";
 import hallRoutes from "./routes/hall.route";
 import { connectDB, MONGO_URI } from "./config";
+import { fillServices } from "./models/service.model";
+import "./models/user.model";
+import "./models/hall.model";
+import "./models/chatroom.model";
+import "./models/reservation.model";
+import "./models/service.model";
+import "./models/message.model";
+import "./models/offer.model";
 
 const app: Express = express();
 const server: http.Server = http.createServer(app);
+
+// fillServices("640e2f4ce32728c2fb845bef");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/user", userRoutes);
 app.use("/api/hall", hallRoutes);
+app.use("/api/reservation", reservationRoutes);
 
 app.use("/", (req, res) => {
   res.status(200).json({ message: "Main route" });
